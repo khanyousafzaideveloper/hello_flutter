2a74f9f6044e205f6c6dcdd7995f9c70e85b0633
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-17 08:56:40 +0500

    test colors and backgoundcolors

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,13 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.lerp(ThemeData.light(), ThemeData.dark(), 0.5),
       title: 'Welcome to Flutter',
       home: Scaffold(
+        backgroundColor: Colors.lightBlue[200],
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          backgroundColor: Colors.pink[300],
+
+          title: const Text('Welcome to Flutter', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.purple, fontStyle: FontStyle.italic, fontSize: 25),),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: Text('Hello World' ,style: TextStyle(fontStyle: FontStyle.normal, color: Colors.brown, fontSize: 30, wordSpacing: 5, backgroundColor: Colors.yellow  ) , ),
         ),
       ),
     );
